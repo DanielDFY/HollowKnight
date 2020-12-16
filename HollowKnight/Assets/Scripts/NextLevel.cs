093aa0f8f7088b57690c3df88331da841b35b6df
DanielDFY <daniel_dfy98@163.com>
2020-12-16 20:05:32 +0800

    update shade effect for scene changing

--- HollowKnight/Assets/Scripts/NextLevel.cs
+++ HollowKnight/Assets/Scripts/NextLevel.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    public float shadeDelay;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject != GlobalController.Instance.player)
-            return;     
+            return;
+        /*
         Text textTime = GameObject.Find("ShowTime").GetComponent<Text>();
         PlayerPrefs.SetString("time", textTime.text);
-        
+        */
+
         // TODO：保存当前关卡，重玩的话重新加载
         PlayerPrefs.SetString("level", SceneManager.GetActiveScene().name);
+
+        // 画面渐暗动画, 结束后切换场景
+        StartCoroutine(shadeCoroutine());
+    }
+
+    private IEnumerator shadeCoroutine()
+    {
+        Image shadePanalImage = GameObject.Find("ShadePanel").GetComponent<Image>();
+
+        while (shadeDelay > 0)
+        {
+            shadeDelay -= Time.deltaTime;
+
+            if (shadePanalImage.color.a < 1)
+            {
+                Color newColor = shadePanalImage.color;
+                newColor.a += Time.deltaTime / shadeDelay;
+                shadePanalImage.color = newColor;
+                yield return null;
+            }
+        }
 
         // PlayerPrefs.SetString("Milestone", GlobalController.Instance.nextScene);
         SceneManager.LoadScene("Victory");
